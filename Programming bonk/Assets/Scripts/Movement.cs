bcf223919686c2e9a4fc2e887bb4322d9134e87c
FlipDark <sanderbenning2007@gmail.com>
2025-01-22 15:30:29 +0100

    added a collision script, raycast and scenes
    added a colission script to check if you hit an obstacle it will "kill" you and switch to endscene, raycast added for detection if your grounded to not have it possible to jump multiple times and added a start and endscene.

--- Programming bonk/Assets/Scripts/Movement.cs
+++ Programming bonk/Assets/Scripts/Movement.cs
@@ -8,11 +8,20 @@ public class Movement : MonoBehaviour
     [SerializeField] private float sidewaySpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private Collider col;
+
+    private float distToGround;
 
     // bools
     private bool pressedA = false;
     private bool pressedD = false;
     private bool pressedSpace = false;
+
+    private void Start()
+    {
+        distToGround = col.bounds.extents.y;
+    }
+
     void Update()
     {
         // Input:
@@ -24,13 +33,15 @@ public class Movement : MonoBehaviour
         // physics
         DoPhysics();
 
+        IsGrounded();
+
         // reset input
         ResetInput();
     }
     private void DoPhysics()
     {
         //jumping
-        if (pressedSpace) rb.AddForce(transform.up * jumpForce);
+        if (pressedSpace && IsGrounded()) rb.AddForce(transform.up * jumpForce);
 
         //move left
         if (pressedA) rb.AddForce(-transform.right * sidewaySpeed);
@@ -56,5 +67,11 @@ public class Movement : MonoBehaviour
         pressedA = false;
         pressedD = false;
         pressedSpace = false;
+    }
+
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0, 1);
     }
 }
